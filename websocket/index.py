62e0f9b67844b6f4042e9620abba6a1278a40282
Meng Lin <menglinmaker@gmail.com>
2023-09-23 00:18:08 +1000

    Refactor to helper functions

--- websocket/index.py
+++ websocket/index.py
@@ -1,76 +1,69 @@
 import json
-import boto3
 import time
+try:
+  import boto3
+except:
+  pass
 
 connectionIds = []
 
+def response(responseCode, body):
+  return json.dumps({ 
+    'statusCode': responseCode,
+    'body': body
+  })
+
 def connectHandler(apigw_management, connectionId):
   for yourConnectionId in connectionIds:
-    data = json.dumps({
-      'statusCode': 200, 
-      'body': {
-        'Your ID': yourConnectionId,
-        'Just connected': connectionId,
-        'Number of connections': len(connectionIds),
-        'Time': f'{time.time()} seconds',
-        'List IDs': connectionIds
-      }
+    data = response(200, {
+      'Your ID': yourConnectionId,
+      'Just connected': connectionId,
+      'Number of connections': len(connectionIds),
+      'Time': f'{time.time()} seconds',
+      'List IDs': connectionIds
     })
     apigw_management.post_to_connection(ConnectionId=connectionId, Data=data)
   
 def disconnectHandler(apigw_management, connectionId):
   for yourConnectionId in connectionIds:
-    data = json.dumps({
-      'statusCode': 200, 
-      'body': {
-        'Your ID': yourConnectionId,
-        'Just disconnected': connectionId,
-        'Number of connections': len(connectionIds),
-        'Time': f'{time.time()} seconds',
-        'List IDs': connectionIds
-      }
+    data = response(200, {
+      'Your ID': yourConnectionId,
+      'Just disconnected': connectionId,
+      'Number of connections': len(connectionIds),
+      'Time': f'{time.time()} seconds',
+      'List IDs': connectionIds
     })
   apigw_management.post_to_connection(ConnectionId=connectionId, Data=data)
 
 def defaultHandler(apigw_management, connectionId):
   for yourConnectionId in connectionIds:
-    data = json.dumps({
-      'statusCode': 200, 
-      'body': {
-        'Your ID': yourConnectionId,
-        'Default ID': connectionId,
-        'Number of connections': len(connectionIds),
-        'Time': f'{time.time()} seconds',
-        'List IDs': connectionIds
-      }
+    data = response(200, {
+      'Your ID': yourConnectionId,
+      'Default ID': connectionId,
+      'Number of connections': len(connectionIds),
+      'Time': f'{time.time()} seconds',
+      'List IDs': connectionIds
     })
   apigw_management.post_to_connection(ConnectionId=connectionId, Data=data)
 
 def handler(event, context):
   connectionId = event.get('requestContext',{}).get('connectionId')
   if connectionId is None:
-    return json.dumps({ 
-      'statusCode': 400, 
-      'body': 'Could not get connection id'
-    })
+    return response(400, 'Request requestContext.connectionId does not exist')
   connectionIds.append(connectionId)
   
-  eventType = event["requestContext"]["eventType"]
-  domain_name = event.get('requestContext',{}).get('domainName')
-  stage = event.get('requestContext',{}).get('stage')
-  apigw_management = boto3.client('apigatewaymanagementapi', endpoint_url=F"https://{domain_name}/{stage}")
- 
   try:
+    eventType = event["requestContext"]["eventType"]
+    domainName = event.get('requestContext',{}).get('domainName')
+    stage = event.get('requestContext',{}).get('stage')
+    apigw_management = boto3.client('apigatewaymanagementapi', endpoint_url=F"https://{domainName}/{stage}")
     if eventType == 'connect':
-      connectHandler(apigw_management, connectionId)
+      return connectHandler(apigw_management, connectionId)
     elif eventType == 'disconnect':
-      disconnectHandler(apigw_management, connectionId)
+      return disconnectHandler(apigw_management, connectionId)
     elif eventType == 'default':
-      defaultHandler(apigw_management, connectionId)
+      return defaultHandler(apigw_management, connectionId)
   except:
-    return json.dumps({ 
-      'statusCode': 400, 
-      'body': 'Server handler could not execute'
-    })
+    return response(400, 'Handler failed to execute')
 
   return
