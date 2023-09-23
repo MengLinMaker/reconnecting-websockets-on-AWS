02383cfc54765bbb524e65d174be6cd7a53a2b09
Meng Lin <menglinmaker@gmail.com>
2023-09-23 10:04:30 +1000

    Fix websocket

--- websocket/index.py
+++ websocket/index.py
@@ -5,65 +5,57 @@ try:
 except:
   pass
 
-connectionIds = []
+startTime = time.time()
+connectionIds = set()
 
 def response(responseCode, body):
-  return json.dumps({ 
+  # Websocket lambda must return status code and stringified body
+  return { 
     'statusCode': responseCode,
-    'body': body
-  })
+    'body': json.dumps(body)
+  }
 
 def connectHandler(apigw_management, connectionId):
-  for yourConnectionId in connectionIds:
-    data = response(200, {
-      'Your ID': yourConnectionId,
-      'Just connected': connectionId,
-      'Number of connections': len(connectionIds),
-      'Time': f'{time.time()} seconds',
-      'List IDs': connectionIds
-    })
-    apigw_management.post_to_connection(ConnectionId=connectionId, Data=data)
+  pass
   
 def disconnectHandler(apigw_management, connectionId):
-  for yourConnectionId in connectionIds:
-    data = response(200, {
-      'Your ID': yourConnectionId,
+  connectionIds.remove(connectionId)
+  for loopConnectionId in list(connectionIds):
+    data = json.dumps({
+      'Your ID': loopConnectionId,
       'Just disconnected': connectionId,
       'Number of connections': len(connectionIds),
-      'Time': f'{time.time()} seconds',
-      'List IDs': connectionIds
+      'Time': f'{time.time() - startTime} seconds',
+      'List IDs': list(connectionIds)
     })
-  apigw_management.post_to_connection(ConnectionId=connectionId, Data=data)
+    apigw_management.post_to_connection(ConnectionId=loopConnectionId, Data=data)
 
 def defaultHandler(apigw_management, connectionId):
-  for yourConnectionId in connectionIds:
-    data = response(200, {
-      'Your ID': yourConnectionId,
-      'Default ID': connectionId,
-      'Number of connections': len(connectionIds),
-      'Time': f'{time.time()} seconds',
-      'List IDs': connectionIds
+  for loopConnectionId in list(connectionIds):
+    data = json.dumps({
+      "Time": time.time() - startTime
     })
-  apigw_management.post_to_connection(ConnectionId=connectionId, Data=data)
+    apigw_management.post_to_connection(ConnectionId=loopConnectionId, Data=data)
 
 def handler(event, context):
-  connectionId = event.get('requestContext',{}).get('connectionId')
+  connectionId = event.get('requestContext', {}).get('connectionId')
   if connectionId is None:
     return response(400, 'Request requestContext.connectionId does not exist')
-  connectionIds.append(connectionId)
-  
+  print('Adding connection')
+  connectionIds.add(connectionId)
+
   try:
     eventType = event["requestContext"]["eventType"]
     domainName = event.get('requestContext',{}).get('domainName')
-    stage = event.get('requestContext',{}).get('stage')
-    apigw_management = boto3.client('apigatewaymanagementapi', endpoint_url=F"https://{domainName}/{stage}")
-    if eventType == 'connect':
-      return connectHandler(apigw_management, connectionId)
-    elif eventType == 'disconnect':
-      return disconnectHandler(apigw_management, connectionId)
-    elif eventType == 'default':
-      return defaultHandler(apigw_management, connectionId)
+    stage = event.get('requestContext', {}).get('stage')
+    apigw_management = boto3.client('apigatewaymanagementapi', endpoint_url=f"https://{domainName}/{stage}")
+    if eventType == 'CONNECT':
+      connectHandler(apigw_management, connectionId)
+    elif eventType == 'DISCONNECT':
+      disconnectHandler(apigw_management, connectionId)
+    elif eventType == 'DEFAULT':
+      defaultHandler(apigw_management, connectionId)
+    
+    return response(200, 'Connected')
   except:
     return response(400, 'Handler failed to execute')
-
-  return
--- websocket/test/test.py
+++ websocket/test/test.py
@@ -11,10 +11,7 @@ class testIndexHandler(unittest.TestCase):
   def test_no_connection_id(self):
     event = {}
     output = index.handler(event, None)
-    expected = json.dumps({ 
-      'statusCode': 400,
-      'body': 'Request requestContext.connectionId does not exist'
-    })
+    expected = index.response(400, 'Request requestContext.connectionId does not exist')
     self.assertEqual(output, expected)
 
   def test_has_connection_id(self):
@@ -24,10 +21,7 @@ class testIndexHandler(unittest.TestCase):
       }
     }
     output = index.handler(event, None)
-    expected = json.dumps({ 
-      'statusCode': 400,
-      'body': 'Handler failed to execute'
-    })
+    expected = index.response(400, 'Handler failed to execute')
     self.assertEqual(output, expected)
 
 if __name__ == '__main__':
