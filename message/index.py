656d18af6a1dff2eca2a1c567f5a7cf6c1285183
Meng Lin <menglinmaker@gmail.com>
2023-10-02 16:28:01 +1100

    Handle errors from dynamo stream extraction

--- message/index.py
+++ message/index.py
@@ -3,11 +3,11 @@ import boto3
 dynamodbTable = boto3.resource('dynamodb').Table('WebSocket-Test-Table')
 
 def handler(event, context):
-  dynamodbNewImage = event['Records'][0]['dynamodb']['NewImage']
-  endpoint_url = dynamodbNewImage['websocketURL']['S']
-  currentID = dynamodbNewImage['currentID']['S']
-  apigateway = boto3.client('apigatewaymanagementapi', endpoint_url=endpoint_url)
   try:
+    dynamodbNewImage = event['Records'][0]['dynamodb']['NewImage']
+    endpoint_url = dynamodbNewImage['websocketURL']['S']
+    currentID = dynamodbNewImage['currentID']['S']
+    apigateway = boto3.client('apigatewaymanagementapi', endpoint_url=endpoint_url)
     message = dynamodbNewImage['message']['S']
     apigateway.post_to_connection(ConnectionId=currentID, Data=message)
   except: pass
