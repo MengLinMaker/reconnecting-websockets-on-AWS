b169a6ba86f687a4a7eed2b9672313129caefee9
Meng Lin <menglinmaker@gmail.com>
2023-10-02 20:20:47 +1100

    Function description

--- message/index.py
+++ message/index.py
@@ -2,12 +2,14 @@ import boto3
 
 dynamodbTable = boto3.resource('dynamodb').Table('WebSocket-Test-Table')
 
+# Websocket message client from dynamodb stream
 def handler(event, context):
   try:
     dynamodbNewImage = event['Records'][0]['dynamodb']['NewImage']
     endpoint_url = dynamodbNewImage['websocketURL']['S']
     currentID = dynamodbNewImage['currentID']['S']
-    apigateway = boto3.client('apigatewaymanagementapi', endpoint_url=endpoint_url)
     message = dynamodbNewImage['message']['S']
+
+    apigateway = boto3.client('apigatewaymanagementapi', endpoint_url=endpoint_url)
     apigateway.post_to_connection(ConnectionId=currentID, Data=message)
   except: pass
