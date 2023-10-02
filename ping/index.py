ac4cbdf695d2b023d214cc5af8bf80a18dd96ddc
Meng Lin <menglinmaker@gmail.com>
2023-10-02 20:25:58 +1100

    Prevent update to non-existent dynamodb row

--- ping/index.py
+++ ping/index.py
@@ -5,8 +5,13 @@ import json
 dynamodbTable = boto3.resource('dynamodb').Table('WebSocket-Test-Table')
 
 def dynamodb_update(dynamodbTable, dynamodbKey, attribute):
+  condition_expression = []
   update_expression = []
   attribute_values = dict()
+  
+  for key, val in dynamodbKey.items():
+    condition_expression.append(f'attribute_exists({key})')
+  condition_expression = ' AND '.join(condition_expression)
 
   for key, val in attribute.items():
     update_expression.append(f' {key} = :{key}')
@@ -16,35 +21,30 @@ def dynamodb_update(dynamodbTable, dynamodbKey, attribute):
   dynamodbTable.update_item(
     Key=dynamodbKey,
     UpdateExpression=update_expression,
-    ExpressionAttributeValues=attribute_values
+    ExpressionAttributeValues=attribute_values,
+    ConditionExpression=condition_expression
   )
 
 def handler(event, context):
   body = json.loads(event['body'])
-  endpoint_url = body['endpoint']
-  parentID = body['parentID']
+  dynamodbKey = {
+    'websocketURL': body['endpoint'],
+    'parentID': body['parentID']
+  }
+
   currentTime = time.time()
   while (time.time() - currentTime < 20):
     message = json.dumps({
       'Time': '{:.1f} sec'.format(time.time() - currentTime)
     })
-    dynamodbKey = {
-        'websocketURL': endpoint_url,
-        'parentID': parentID
-      }
-    dynamodb_update(dynamodbTable, dynamodbKey, {
-      'message': message
-    })
-    time.sleep(1)
+    dynamodb_update(dynamodbTable, dynamodbKey, {'message': message})
+    time.sleep(2.5)
 
   response = {
     'statusCode': 200,
     'headers': {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': 'true',
-    },
-    'body': json.dumps({
-      'message': 'Success'
-    })
+    }
   }
   return response
